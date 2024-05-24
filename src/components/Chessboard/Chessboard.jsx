0568986d0a9cb1b79a68c2502702f9239108d1f9
harshitChauhan001 <108900696+harshitChauhan001@users.noreply.github.com>
2024-05-24 23:16:01 +0530

    Update Chessboard.jsx

--- src/components/Chessboard/Chessboard.jsx
+++ src/components/Chessboard/Chessboard.jsx
@@ -33,7 +33,7 @@ const Chessboard = ({ UniqueKey }) => {
   const navigate = useNavigate();
 
 
-     const socket = io('http://localhost:3001');
+     const socket = io('https://chess-redis-server.onrender.com/');
     useEffect(() => {
       socket.emit('join', UniqueKey);
   
